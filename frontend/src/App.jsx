079ec8b62a9f25bd015e2818923409e7ccdb4b35
Vikranth2024 <vikranthsabari436@gmail.com>
2025-05-14 00:41:25 +0530

    frontend initialized

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="bg-orange-400 p-3 text-4xl flex justify-center items-center">Welcome to Vidyapaalam</h1>
+    </>
+  )
+}
+
+export default App
