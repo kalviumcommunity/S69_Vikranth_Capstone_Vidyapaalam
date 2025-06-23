ddcbd7a9625ec13fefbd29861e7ef7743b48b4dc
Vikranth2024 <vikranthsabari436@gmail.com>
2025-06-24 01:56:49 +0530

    issues updated

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,114 +2,117 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
 const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    sparse: true,
-  },
-  password: {
-    type: String,
-  },
-  googleId: {
-    type: String,
-    unique: true,
-    sparse: true,
-  },
-  isGoogleUser: { type: Boolean, default: false },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    sparse: true,
+  },
+  password: {
+    type: String,
+    select: false, 
+  },
+  googleId: {
+    type: String,
+    unique: true,
+    sparse: true,
+  },
+  isGoogleUser: { type: Boolean, default: false },
 
-  googleCalendar: {
-    refreshToken: {
-      type: String,
-      default: null,
-      select: false,
-    },
-    accessToken: { // <-- NEW FIELD: To store the short-lived access token
-      type: String,
-      default: null,
-      select: false,
-    },
-    accessTokenExpiryDate: { // <-- NEW FIELD: To store when the access token expires
-      type: Date,
-      default: null,
-      select: false,
-    },
-    connected: {
-      type: Boolean,
-      default: false,
-    },
-    lastConnected: {
-      type: Date,
-    },
-  },
-  bio: {
-    type: String,
-    default: "",
-  },
-  phoneNumber: {
-    type: String,
-    trim: true,
-    default: "",
-  },
-  role: {
-    type: String,
-    enum: ["student", "teacher", "admin"],
-    default: "teacher",
-  },
-  activeToken: {
-    type: String,
-    default: null,
-  },
-  refreshToken: {
-    type: String,
-    default: null,
-  },
-  resetPasswordToken: {
-    type: String,
-    default: null,
-  },
-  resetPasswordExpire: {
-    type: Date,
-    default: null,
-  },
-  isVerified: {
-    type: Boolean,
-    default: false,
-  },
-  verificationToken: {
-    type: String,
-    default: null,
-  },
-  interestedSkills: [{
-    type: String,
-    trim: true,
-  }],
-  teachingSkills: [{
-    type: String,
-    trim: true,
-  }],
+  googleCalendar: {
+    refreshToken: {
+      type: String,
+      default: null,
+      select: false,
+    },
+    accessToken: {
+      type: String,
+      default: null,
+      select: false,
+    },
+    accessTokenExpiryDate: {
+      type: Date,
+      default: null,
+      select: false,
+    },
+    connected: {
+      type: Boolean,
+      default: false,
+    },
+    lastConnected: {
+      type: Date,
+    },
+  },
+  bio: {
+    type: String,
+    default: "",
+  },
+  phoneNumber: {
+    type: String,
+    trim: true,
+    default: "",
+  },
+  role: {
+    type: String,
+    enum: ["student", "teacher", "admin"],
+    default: "teacher",
+  },
+  activeToken: {
+    type: String,
+    default: null,
+    select: false, 
+  },
+  refreshToken: {
+    type: String,
+    default: null,
+    select: false, 
+  },
+  resetPasswordToken: {
+    type: String,
+    default: null,
+  },
+  resetPasswordExpire: {
+    type: Date,
+    default: null,
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  verificationToken: {
+    type: String,
+    default: null,
+  },
+  interestedSkills: [{
+    type: String,
+    trim: true,
+  }],
+  teachingSkills: [{
+    type: String,
+    trim: true,
+  }],
 
-  availability: {
-    date: {
-      type: Date,
-    },
-    slots: [{
-      type: String,
-      trim: true,
-    }],
-  }
+  availability: {
+    date: {
+      type: Date,
+    },
+    slots: [{
+      type: String,
+      trim: true,
+    }],
+  }
 });
 
 UserSchema.pre("save", async function (next) {
-  if (this.isModified("password") && this.password) {
-    const salt = await bcrypt.genSalt(12);
-    this.password = await bcrypt.hash(this.password, salt);
-  }
-  next();
+  if (this.isModified("password") && this.password) {
+    const salt = await bcrypt.genSalt(12);
+    this.password = await bcrypt.hash(this.password, salt);
+  }
+  next();
 });
 
 module.exports = mongoose.model("User", UserSchema);
