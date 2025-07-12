ed9a75a55aa58b5b64d00fc234de0c37f80e1776
Vikranth2024 <167829581+Vikranth2024@users.noreply.github.com>
2025-07-12 15:27:15 +0530

    Create uploadMiddleware.js

---
+++ backend/middleware/uploadMiddleware.js
@@ -0,0 +1,29 @@
+    // middleware/uploadMiddleware.js
+
+    const multer = require('multer');
+
+    const storage = multer.memoryStorage(); 
+
+    const fileFilter = (req, file, cb) => {
+      if (file.mimetype.startsWith('image/') || file.mimetype.startsWith('video/')) {
+        cb(null, true);
+      } else {
+        cb(new Error('Only image and video files are allowed!'), false);
+      }
+    };
+
+
+    const upload = multer({
+      storage: storage,
+      fileFilter: fileFilter,
+      limits: {
+        fileSize: 10 * 1024 * 1024 
+      }
+    }).fields([
+      { name: 'avatar', maxCount: 1 },
+      { name: 'video', maxCount: 1 },
+      { name: 'galleryPhotos', maxCount: 10 } 
+    ]);
+
+    module.exports = upload;
+    
