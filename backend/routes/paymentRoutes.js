361ad4da0094823cfeca95802d0b9a04c3cbe374
Vikranth2024 <167829581+Vikranth2024@users.noreply.github.com>
2025-07-25 17:38:29 +0530

    Update paymentRoutes.js

--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const {createPaymentOrder,handleRazorpayWebhook} = require("../controller/paymentController");
-const {protect} = require("../middleware/auth");
+const {protect} = require("../middleware/authMiddleware");
 
 router.post("/create-payment-order", protect, createPaymentOrder);
 router.post("/razorpay-webhook", handleRazorpayWebhook);
