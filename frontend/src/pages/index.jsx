34ddf62c71dfa6fb76a6c755e709ea3b80ca5283
Vikranth2024 <167829581+Vikranth2024@users.noreply.github.com>
2025-07-28 17:29:57 +0530

    Update index.jsx

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -2,6 +2,7 @@ import HeroSection from "@/components/home/HeroSection";
 import FeaturedSkills from "@/components/home/FeaturedSkills";
 import HowItWorks from "@/components/home/HowItWorks";
 import TestimonialsSection from "@/components/home/TestimonialsSection";
+import Features from "@/components/home/Features";
 
 const Index = () => {
   return (
@@ -9,6 +10,7 @@ const Index = () => {
       
       <HeroSection />
       <FeaturedSkills />
+      <Features/>
       <HowItWorks />
       <TestimonialsSection />
     </div>
