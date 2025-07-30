c2b2f1ff8b7688e729769f20d38b1168305b92f0
Vikranth2024 <167829581+Vikranth2024@users.noreply.github.com>
2025-07-30 22:32:15 +0530

    Update sessionRoutes.js

--- backend/routes/sessionRoutes.js
+++ backend/routes/sessionRoutes.js
@@ -2,11 +2,14 @@ const express = require("express");
 const router = express.Router();
 const {
     getSessions,
-    createSession
+    createSession,
+    getTeacherSessions
 } = require("../controller/sessionController");
 const {protect} = require("../middleware/authMiddleware"); // Ensure this exists
 
 router.get("/student/sessions", protect, getSessions);
 router.post("/student/sessions", protect, createSession);
+router.get("/teacher/sessions", protect, getTeacherSessions);
+
 
 module.exports = router;
